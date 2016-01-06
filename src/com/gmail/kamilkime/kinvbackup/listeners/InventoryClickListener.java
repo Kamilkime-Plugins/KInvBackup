18bec12c07824e48d5441399cb385b17c843c2d4
Robin Chiu <robin@hulua.cc>
2016-01-06 14:07:56 +0800

    refactor inventory title name to a constant

--- src/com/gmail/kamilkime/kinvbackup/listeners/InventoryClickListener.java
+++ src/com/gmail/kamilkime/kinvbackup/listeners/InventoryClickListener.java
@@ -9,13 +9,15 @@ import com.gmail.kamilkime.kinvbackup.utils.StringUtils;
 
 public class InventoryClickListener implements Listener {
 
+	private static final String INV_TITLE = StringUtils.color("&5&lKInvBackup backup display");
+
 	@EventHandler
 	public void onClick(InventoryClickEvent e){
 		if(e.getInventory() == null) return;
 		if(e.getCurrentItem() == null) return;
 		if(e.getCurrentItem().getType().equals(Material.AIR)) return;
 		if(e.getInventory().getTitle() == null) return;
-		if(e.getInventory().getTitle().equals(StringUtils.color("&5&lKInvBackup backup display"))){
+		if(e.getInventory().getTitle().equals(INV_TITLE)){
 			e.setCancelled(true);
 			e.getWhoClicked().openInventory(e.getInventory());
 		}
